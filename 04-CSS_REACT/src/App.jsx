db3503a6a7ca06eb20064928db10b091517921ed
KennethOlusegun <88892775+KennethOlusegun@users.noreply.github.com>
2023-01-22 11:19:10 -0300

    chore: understanding component css

--- 04-CSS_REACT/src/App.jsx
+++ 04-CSS_REACT/src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css';
+import './App.css'
+import { MyComponent } from './components/MyComponent'
 
 export default function App() {
   return (
     <div className="App">
       <h1>Css no React</h1>
+      {/* css de componente */}
+      <MyComponent />
+      <p>Este paragráfo é do App.js</p>
     </div>
-  );
+  )
 }
-
-
---
+++ 04-CSS_REACT/src/components/MyComponent.jsx
@@ -0,0 +1,11 @@
+import './MyComponent.css'
+
+export const MyComponent = () => {
+  return (
+    <div>
+      <h1>Css de componente</h1>
+      <p>Este é o paragráfo do componente</p>
+      <p className="my-comp-paragraph">Este também é do componente</p>
+    </div>
+  )
+}
