7d9ded2e8b24a0ccf13de8194b7fac05b3aa2ccf
KennethOlusegun <88892775+KennethOlusegun@users.noreply.github.com>
2023-01-22 14:19:34 -0300

    chore: understanding form creation

---
+++ 05-FORMULARIOS_NO_REACT/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import MyForms from './components/MyForms'
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2>Forms</h2>
+      <MyForms />
+    </div>
+  )
+}
