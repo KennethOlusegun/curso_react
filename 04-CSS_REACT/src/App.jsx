6c371f2759cafee84222f2e9a35c584e7dd22d3d
KennethOlusegun <88892775+KennethOlusegun@users.noreply.github.com>
2023-01-22 11:44:09 -0300

    chore: understanding inline dynamic css

--- 04-CSS_REACT/src/App.jsx
+++ 04-CSS_REACT/src/App.jsx
@@ -1,7 +1,11 @@
-import './App.css'
-import { MyComponent } from './components/MyComponent'
+import "./App.css"
+import { MyComponent } from "./components/MyComponent"
+import { useState } from "react"
 
 export default function App() {
+  const n = 15
+  const [name] = useState("Kenneth")
+
   return (
     <div className="App">
       <h1>Css no React</h1>
@@ -9,9 +13,21 @@ export default function App() {
       <MyComponent />
       <p>Este paragráfo é do App.js</p>
       {/* css inline*/}
-      <p style={{ color: 'blue', padding: "25px", borderTop: "2px solid red" }}>
+      <p style={{ color: "blue", padding: "25px", borderTop: "2px solid red" }}>
         Este elemento foi estilizado de forma inline
       </p>
+      {/* css inline dinâmico */}
+      <h2 style={n < 10 ? { color: "purple" } : { color: "pink" }}>
+        Css dinâmico
+      </h2>
+      <h2
+        style={
+          name === "Kenneth"
+            ? { color: "green", backgroundColor: "#000" }
+            : null}
+      >
+        Css dinâmico
+      </h2>
     </div>
   )
 }
