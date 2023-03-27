575f6f66fe45d60de98f578c90f73f88c6c4b502
KennethOlusegun <kennetholusegun@gmail.com>
2023-03-27 12:38:22 -0300

    chore: setting the functions of game

--- 06-projeto_secret_world/src/App.js
+++ 06-projeto_secret_world/src/App.js
@@ -1,7 +1,13 @@
-import { useCallback, useEffect, useState } from 'react'
+// css
 import './App.css'
+
+// react
 import { useCallback, useEffect, useState } from 'react'
+
+// data
 import { wordsList } from './data/words'
+
+//components
 import StartScreen from './components/StartScreen'
 import Game from './components/Game'
 import GameOver from './components/GameOver'
@@ -13,15 +19,29 @@ const stages = [
 ]
 
 function App() {
-  const [gameStage, setGameStage] = useState(stage[0].name)
+  const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
-  console.log(words)
+
+  //starts the secret  word game
+	const startGame = () => {
+	setGameStage(stages[1].name)
+  }
+
+  // process the letter input
+  const verifyLetter = () => {
+	setGameStage(stages[2].name)
+  }
+
+  // restarts the game
+  const retry = () => {
+	setGameStage(stages[0].name)
+  }
 
   return (
     <div className="App">
-      {gameStage === 'start' && <StartScreen />}
-      {gameStage === 'game' && <Game />}
-      {gameStage === 'end' && <GameOver />}
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'end' && <GameOver retry={retry}/>}
     </div>
   )
 }
